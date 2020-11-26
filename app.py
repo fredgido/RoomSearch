70478ff64075ab7b47130a446b04e156aff9eba4
Fred Fernandes <fred.fernandes@neonumy.com>
2020-11-26 19:42:22 +0000

    sql alchemy models

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+
+
+
+db = SQLAlchemy(app)
+
+
+class Floor(db.Model):
+    __tablename__ = 'floor'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    image_file = db.Column(db.String(256))
+    info = db.Column(db.Text)
+
+    def __repr__(self):
+        return repr(id)
+
+
+class Room(db.Model):
+    __tablename__ = 'room'
+    id = db.Column(db.Integer, primary_key=True)
+    map_position_x = db.Column(db.Float)
+    map_position_y = db.Column(db.Float)
+    room_type = db.Column(db.String(256))
+    capacity = db.Column(db.Integer)
+    schedule_file = db.Column(db.String(256))
+    info = db.Column(db.Text)
+
+    floor_id = db.Column(db.Integer, db.ForeignKey(Floor.id))
+
+    def __repr__(self):
+        return repr(id)
+
+
+db.create_all()
 
 
 @app.route('/')

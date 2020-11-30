f457d5114158500e1b9559f6fe7b03e1d22449a1
Fred Fernandes <fred.fernandes@neonumy.com>
2020-11-30 20:02:20 +0000

    outra sala

--- app.py
+++ app.py
@@ -54,10 +54,24 @@ def floorlist():
 @app.route('/room', methods=['GET'])
 def room_get():
     name = request.args.get('search')
-    r = Room.query.filter_by(name=name).all()[0]
+    r = Room.query.filter_by(name=name).all()
+    if len(r) < 1:
+        return jsonify({}), 404
+    r = r[0]
     return jsonify({'id': r.id, 'room_type': r.room_type, 'map_position_x': r.map_position_x,
                     'map_position_y': r.map_position_y,'capacity':r.capacity
                     } )
+
+
+@app.route("/room", methods=['POST'])
+def item_save():
+    try:
+        new_item = Room(**request.form)
+        db.session.add(new_item)
+        db.session.commit()
+    except BaseException as error:
+        return str(error), 400
+    return "success", 200
 
 
 if __name__ == '__main__':
